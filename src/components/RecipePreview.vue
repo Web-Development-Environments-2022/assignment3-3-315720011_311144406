9116151055f58e095bc2d78606085b4275741e94
tomerbv <tomerbv@gmail.com>
2022-08-11 16:26:49 +0300

    add image link and cursor

--- src/components/RecipePreview.vue
+++ src/components/RecipePreview.vue
@@ -1,7 +1,12 @@
 <template>
 
   <div class="card">
-    <img v-if="image_load" :src="recipe.image" class="card-img-top" />
+    <img 
+      v-if="image_load" 
+      :src="recipe.image" 
+      class="card-img-top" 
+      style="cursor: pointer;" 
+      @click="showRecipe()"/>
     <div class="card-body">
       <h5 class="card-title">{{ recipe.title }}</h5>
     </div>
@@ -15,7 +20,7 @@
       </li>
     </ul>
     <div class="card-footer" style="bottom=0">
-      <b-button class="card-link" :to="{ name: 'recipe', params: { recipeId: recipe.id } }" type="submit">Show Recipe</b-button>
+      <b-button class="card-link" @click="showRecipe()" type="submit">Show Recipe</b-button>
       <ToggleFavorite 
         id="favo"
         v-if="this.showFavorite"
@@ -80,6 +85,10 @@ export default {
       } catch (err) {
         console.log(err.response);
       }      
+    },
+
+    showRecipe(){
+      this.$router.push({ name: 'recipe', params: { recipeId: this.recipe.id } });
     }
   },
   filters:{
@@ -91,80 +100,6 @@ export default {
 </script>
 
 <style scoped>
-.recipe-preview {
-  display: inline-block;
-  width: 90%;
-  height: 100%;
-  position: relative;
-  margin: 10px 10px;
-}
-.recipe-preview > .recipe-body {
-  width: 100%;
-  height: 200px;
-  position: relative;
-}
-
-.recipe-preview .recipe-body .recipe-image {
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: auto;
-  margin-bottom: auto;
-  display: block;
-  width: 98%;
-  height: auto;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  background-size: cover;
-}
-
-.recipe-preview .recipe-footer {
-  width: 100%;
-  height: 50%;
-  overflow: hidden;
-}
-
-.recipe-preview .recipe-footer .recipe-title {
-  padding: 10px 10px;
-  width: 100%;
-  font-size: 12pt;
-  text-align: left;
-  white-space: nowrap;
-  overflow: hidden;
-  -o-text-overflow: ellipsis;
-  text-overflow: ellipsis;
-}
-
-.recipe-preview .recipe-footer ul.recipe-overview {
-  padding: 5px 10px;
-  width: 100%;
-  display: -webkit-box;
-  display: -moz-box;
-  display: -webkit-flex;
-  display: -ms-flexbox;
-  display: flex;
-  -webkit-box-flex: 1;
-  -moz-box-flex: 1;
-  -o-box-flex: 1;
-  box-flex: 1;
-  -webkit-flex: 1 auto;
-  -ms-flex: 1 auto;
-  flex: 1 auto;
-  table-layout: fixed;
-  margin-bottom: 0px;
-}
-
-.recipe-preview .recipe-footer ul.recipe-overview li {
-  -webkit-box-flex: 1;
-  -moz-box-flex: 1;
-  -o-box-flex: 1;
-  -ms-box-flex: 1;
-  box-flex: 1;
-  -webkit-flex-grow: 1;
-  flex-grow: 1;
-  width: 90px;
-  display: table-cell;
-  text-align: center;
-}
 
 #favo{
   margin-left: 120px;
